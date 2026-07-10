843264b1dfabf1e23fd1581ba9820c0bdb335eca
shotwn <anilhaksever@gmail.com>
2026-07-10 15:18:06 -0400

    chore: js linting

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 8 */
+
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom"
